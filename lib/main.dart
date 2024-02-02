84e988c75cfd19e45fa652e42244bea74583799d
Declan Brown <dbrown39@nd.edu>
2024-02-02 14:45:46 -0500

    Trying to get firebase to work. We are missing root level build.gradle file. Watching this youtube video: https://www.youtube.com/watch?v=YVB94s21jD8&list=PL5ORE0bGpnzO2EeflxC_R87TxcM1jXPlM please help!

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 // Add useless comment!
 
+import 'package:atlas/firebase_options.dart';
 import 'package:atlas/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:json_theme/json_theme.dart';
 
 import 'package:flutter/services.dart'; // For rootBundle
-import 'dart:convert'; // 
+import 'dart:convert'; //
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   final themeStr = await rootBundle.loadString('assets/appainter_theme.json');
   final themeJson = jsonDecode(themeStr);
   final theme = ThemeDecoder.decodeThemeData(themeJson)!;
